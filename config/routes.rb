058d579a09582c2b46200c9d3df82ee9ef0ae5e8
Stian Johansen <stian.a.johansen@gmail.com>
2021-01-24 22:47:24 +0100

    added devise and token auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  namespace :api do
+  devise_for :users
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :skills
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
